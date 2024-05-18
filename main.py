5e6de3327fbc626e1bac368c8fd3602e5b38aafd
indramhaarta <indramhrt@gmail.com>
2024-05-18 16:32:06 +0700

    fix: add db as prefix for auth

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tryout.router import router as tryout_router
 
 app = FastAPI()
 
-app.include_router(auth_router, prefix="/api/auth")
+app.include_router(auth_router, prefix="/api/db/auth")
 app.include_router(tryout_router, prefix="/api/db/tryout")
 
 @app.get("/api/auth/health")
