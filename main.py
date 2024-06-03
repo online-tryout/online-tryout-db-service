4dc369495e86bbee7ecabb631f453ae7edf9deea
ana117 <63540951+ana117@users.noreply.github.com>
2024-06-03 14:04:39 +0700

    feat: add payment router

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from auth.router import router as auth_router
 from tryout.router import router as tryout_router
+from payment.router import router as payment_router
 
 app = FastAPI()
 
 app.include_router(auth_router, prefix="/api/db/auth")
 app.include_router(tryout_router, prefix="/api/db/tryout")
+app.include_router(payment_router, prefix="/api/db/payment")
 
 @app.get("/api/db/health")
 async def main():
